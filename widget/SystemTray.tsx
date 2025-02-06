be78543f0fc3127fbfa187322733d7597c603f29
Boris Dudelsack <bdudelsack@progeek.de>
2025-02-06 10:11:15 +0100

    fix: system tray menu

--- widget/SystemTray.tsx
+++ widget/SystemTray.tsx
@@ -1,12 +1,10 @@
 import { bind } from "astal"
-import { Astal, Gdk } from "astal/gtk3"
+import { Gtk } from "astal/gtk3"
 import Tray from "gi://AstalTray"
 
 export default function SystemTray() {
     const tray = Tray.get_default()
 
-    const handleClick = (item: Tray.TrayItem) => (_, e: Astal.ClickEvent) => {
-    }
 
     const items = bind(tray, "items").as(items => items.map(
         item => (
@@ -16,23 +14,13 @@ export default function SystemTray() {
                 tooltipMarkup={bind(item, "tooltipMarkup")}
                 actionGroup={bind(item, "actionGroup").as(ag => ["dbusmenu", ag])}
                 menuModel={bind(item, "menuModel")}
-            // onClick={handleClick(item)}
+                direction={Gtk.ArrowType.DOWN}
+                usePopover={false}
             >
                 <icon gicon={bind(item, "gicon")} className="systemtray-icon" />
             </menubutton>
         )
     ))
-
-    // return <box className="SysTray">
-    //     {bind(tray, "items").as(items => items.map(item => (
-    //         <menubutton
-    //             tooltipMarkup={bind(item, "tooltipMarkup")}
-    //             actionGroup={bind(item, "actionGroup").as(ag => ["dbusmenu", ag])}
-    //             menuModel={bind(item, "menuModel")}>
-    //             <icon gicon={bind(item, "gicon")} />
-    //         </menubutton>
-    //     )))}
-    // </box>
 
     return (
         <box className="systemtray box p-1">
